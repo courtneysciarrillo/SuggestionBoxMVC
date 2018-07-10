e47d06392f8f82a28e4506f9190b01ee625d2f8f
courtneysciarrillo <courtney.sciarrillo@gmail.com>
2018-07-10 14:46:54 -0400

    first commit

---
+++ SuggestionBox/Models/SuggestionModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuggestionBox.Models
+{
+    public class SuggestionModel
+    {
+        public string Topic { get; set; }
+
+        [DisplayName("Suggestion Comments")]
+        public string Suggestion { get; set; }
+
+        [Key]
+        public int RecordNum { get; set; }
+    }
+}
